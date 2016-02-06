54ffd90eb644f728d6b8c110851fba2803f7814c
CarlosGabaldon <cgabaldon@gmail.com>
2016-02-06 09:14:55 -0700

    Add link to home page for viewing doctors

--- spec/features/listing_doctors_spec.rb
+++ spec/features/listing_doctors_spec.rb
@@ -1,15 +1,21 @@
 require 'rails_helper'
 
-RSpec.feature "Users can view a list of doctors" do
+RSpec.feature "Patients can view a list of doctors" do
+  let(:user) { create(:user) }
+
   before do
-    @trails = [
+    @patient = create(:patient, user: user)
+    @doctors = [
       create(:doctor, last_name: "Smith"),
       create(:doctor, last_name: "Jones"),
       create(:doctor, last_name: "White")
     ]
+    login_as user
   end
-  scenario "when visting the doctors listing page" do
-    visit doctors_path
+  
+  scenario 'when visting the patient listing page' do
+    visit '/'
+    click_link "Doctors"
     expect(page).to have_content "Doctors"
     expect(page).to have_content "Smith"
     expect(page).to have_content "Jones"

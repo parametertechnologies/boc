2338cf52c61a52015e5647013c6800f7cda0a2d5
CarlosGabaldon <cgabaldon@gmail.com>
2016-02-06 09:11:43 -0700

    Add example for not signed in patients

--- spec/features/identifying_patients_spec.rb
+++ spec/features/identifying_patients_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 RSpec.feature 'Patients are identified by user' do
   let(:user) { create(:user) }
-  before do
-    login_as user
-    @patient = create(:patient, user: user)
-  end
 
-  scenario 'when signed in'do
+  scenario 'when signed in' do
+    login_as user
+    patient = create(:patient, user: user)
     visit '/'
     click_link 'Profile'
-    expect(page).to have_content @patient.full_name
-    expect(page).to have_content @patient.contact_phone
+    expect(page).to have_content patient.full_name
+    expect(page).to have_content patient.contact_phone
   end
+
+  scenario 'when not signed in' do
+    visit '/'
+    expect(page).to have_no_content 'Profile'
+  end
+
 end
